0091e32213d09a3da87d5cf3ea12dd182e6ba7f6
ishikawa <ishikawa@farend.jp>
2022-06-03 15:37:48 +0900

    test

--- .github/scripts/setup-redmica.sh
+++ .github/scripts/setup-redmica.sh
@@ -5,18 +5,18 @@ database=$2
 redmine_dir=$3
 
 mkdir -p $redmine_dir
-cd $redmine_dir/
-wget -O redmine.tar.gz "https://github.com/redmica/redmica/archive/v$redmica_version.tar.gz"; \
+cd $redmine_dir
+wget -O redmine.tar.gz "https://github.com/redmica/redmica/archive/$redmica_version.tar.gz"; \
 tar -xf redmine.tar.gz --strip-components=1;
 
 cp -r $GITHUB_WORKSPACE ./plugins
 cp ./plugins/redmica_ui_extension/.github/templates/database-$database.yml config/database.yml
 cp ./plugins/redmica_ui_extension/.github/templates/application_system_test_case.rb test/application_system_test_case.rb
 
-pwd
+ls
 
-apt-get update; \
-  apt-get install -y --no-install-recommends \
+apt-get update;
+apt-get install -y --no-install-recommends \
     ca-certificates \
     curl \
     wget \
@@ -31,7 +31,13 @@ apt-get update; \
     gsfonts \
     imagemagick \
     shared-mime-info \
-  ; \
+    default-libmysqlclient-dev \
+    freetds-dev \
+    gcc \
+    libpq-dev \
+    libsqlite3-dev \
+    make \
+    patch;
 
 # PDFのサムネイル作成テストを成功させるため
 sed -i 's/^.*policy.*coder.*none.*PDF.*//' /etc/ImageMagick-6/policy.xml
